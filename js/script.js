2179926488355c4394f216d79618e67747beab64
Anna Carolina <the4nna@outlook.com>
2022-05-20 10:56:57 -0300

    finalizando projeto

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const productsContainer = document.getElementById("productsContainer")
 const registerForm = document.getElementById("registerForm")
 const shareForm = document.getElementById("shareForm")
 
-registerForm.addEventListener('submit', function(e) {
+registerForm.addEventListener("submit", function (e) {
     sendData(e)
 })
 
@@ -10,48 +10,48 @@ function sendData(e) {
     e.preventDefault()
 
     let haveError = false
-    let inputName = document.forms['registerForm']['name']
+    let inputName = document.forms["registerForm"]["name"]
 
-    if(!inputName.value) {
+    if (!inputName.value) {
         haveError = true
-        inputName.classList.add('inputError')
+        inputName.classList.add("inputError")
     } else {
-        inputName.classList.remove('inputError')
+        inputName.classList.remove("inputError")
     }
 
-    let inputEmail = document.forms['registerForm']['email']
+    let inputEmail = document.forms["registerForm"]["email"]
 
-    if(!inputEmail.value) {
+    if (!inputEmail.value) {
         haveError = true
-        inputEmail.classList.add('inputError')
+        inputEmail.classList.add("inputError")
     } else {
-        inputEmail.classList.remove('inputError')
+        inputEmail.classList.remove("inputError")
     }
 
-    let inputCPF = document.forms['registerForm']['cpf']
+    let inputCPF = document.forms["registerForm"]["cpf"]
 
-    if(!inputCPF.value) {
+    if (!inputCPF.value) {
         haveError = true
-        inputCPF.classList.add('inputError')
+        inputCPF.classList.add("inputError")
     } else {
-        inputCPF.classList.remove('inputError')
+        inputCPF.classList.remove("inputError")
     }
 
-    let selectGender = document.forms['registerForm']['gender']
+    let selectGender = document.forms["registerForm"]["gender"]
 
-    if(!selectGender.value) {
+    if (!selectGender.value) {
         haveError = true
-        selectGender.classList.add('inputError')
+        selectGender.classList.add("inputError")
     } else {
-        selectGender.classList.remove('inputError')
+        selectGender.classList.remove("inputError")
     }
 
-    if(!haveError) {
+    if (!haveError) {
         registerForm.submit()
     }
 }
 
-shareForm.addEventListener('submit', function(e) {
+shareForm.addEventListener("submit", function (e) {
     friendData(e)
 })
 
@@ -59,105 +59,102 @@ function friendData(e) {
     e.preventDefault()
 
     let haveError = false
-    let inputName = document.forms['shareForm']['name']
+    let inputName = document.forms["shareForm"]["name"]
 
-    if(!inputName.value) {
+    if (!inputName.value) {
         haveError = true
-        inputName.classList.add('inputError')
+        inputName.classList.add("inputError")
     } else {
-        inputName.classList.remove('inputError')
+        inputName.classList.remove("inputError")
     }
 
-    let inputEmail = document.forms['shareForm']['email']
+    let inputEmail = document.forms["shareForm"]["email"]
 
-    if(!inputEmail.value) {
+    if (!inputEmail.value) {
         haveError = true
-        inputEmail.classList.add('inputError')
+        inputEmail.classList.add("inputError")
     } else {
-        inputEmail.classList.remove('inputError')
+        inputEmail.classList.remove("inputError")
     }
-    if(!haveError) {
+    if (!haveError) {
         registerForm.submit()
     }
 }
 
-
 function htmlProducts(data) {
     const productList = data.products
-    productsContainer.innerHTML = ''
-    productList.forEach(product => {
+    productsContainer.innerHTML = ""
+    productList.forEach((product) => {
+        const productDiv = document.createElement("div")
+        productDiv.classList.add("productDiv")
 
-        const productDiv = document.createElement('div')
-        productDiv.classList.add('productDiv')
-    
         productDiv.innerHTML = `
         <img src="${product.image}" class="productImg">  <img> 
         <h3 class="productName"> ${product.name} </h3> 
         <p class="productDescription"> ${product.description} </p>
-        <span class="productPrice"> <s> $ ${product.oldPrice}.00 </s> </span> <br>
-        <span class="productPrice"> $ ${product.price}.00 </span> 
+        <div class="productPrice"> <s> $ ${product.oldPrice}.00 </s> </div>  <br>
+        <div class="productPrice"> $ ${product.price}.00 </div> 
         <button class="buyButton"> Comprar </button>
         `
 
-        const installmentsDiv = document.createElement('div')
-        installmentsDiv.style.display = 'none'
+        const installmentsDiv = document.createElement("div")
+        installmentsDiv.classList.add("installmentsDiv")
+        installmentsDiv.style.display = "none"
         productDiv.insertBefore(installmentsDiv, productDiv.children[4])
-        installmentsDiv.innerHTML = `<span class="installmentSpan"> ou ${product.installments.count} de R$${product.installments.value} </span>`
-        installmentsDiv.addEventListener('click', function() {
-            installmentsDiv.style.display = 'none'
+        installmentsDiv.innerHTML = `<span class="installmentSpan"> ou ${product.installments.count}x <br> de R$${product.installments.value} </span> `
+        installmentsDiv.addEventListener("click", function () {
+            productDiv.children[5].style.display = "inline"
+            productDiv.children[6].style.display = "block"
+            productDiv.children[7].style.display = "inline"
+            installmentsDiv.style.display = "none"
         })
 
-        
-        const buttonInstallment = document.createElement('button')
-        buttonInstallment.classList.add('installmentButton')
+        const buttonInstallment = document.createElement("button")
+        buttonInstallment.classList.add("installmentButton")
         buttonInstallment.innerHTML = '<i class="fa-solid fa-percent"></i>'
         productDiv.appendChild(buttonInstallment)
-        buttonInstallment.addEventListener('click', function() {
-            productDiv.children[4].style.display = 'none'
-            installmentsDiv.style.display = 'block'
+        buttonInstallment.addEventListener("click", function () {
+            productDiv.children[5].style.display = "none"
+            productDiv.children[6].style.display = "none"
+            productDiv.children[7].style.display = "none"
+            installmentsDiv.style.display = "block"
         })
 
         productsContainer.appendChild(productDiv)
-    });
+    })
 }
-
-
 
 function errorEvt() {
-    alert('Aconteceu um erro')
+    alert("Aconteceu um erro")
 }
-
-
 
 let page = 1
 function loadMore() {
     page++
-    urlLoad(page)
-    .then(htmlProducts)
+    urlLoad(page).then(htmlProducts)
 }
 
-
-window.onload = (e) =>
-    e.preventDefault()
-    urlLoad(1)
-    .then(htmlProducts)
-    .catch(errorEvt)
-
+window.onload = (e) => e.preventDefault()
+urlLoad(1).then(htmlProducts).catch(errorEvt)
 
 function urlLoad(pageNumber) {
     return new Promise((resolve, reject) => {
         const xhttp = new XMLHttpRequest()
 
-        xhttp.onreadystatechange = function() {
-            if(this.readyState == 4 && this.status == 200) {
-                const response = (JSON.parse(this.responseText))
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                const response = JSON.parse(this.responseText)
                 resolve(response)
             } else if (this.status === 404) {
                 reject()
             }
         }
-        
-        xhttp.open('GET', `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${pageNumber}`, true)
+
+        xhttp.open(
+            "GET",
+            `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${pageNumber}`,
+            true
+        )
         xhttp.send()
     })
 }
